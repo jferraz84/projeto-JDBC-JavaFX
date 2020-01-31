e3690f2b728fa1d6b272829ee8f1bda0bb848efe
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-01-31 12:26:53 -0300

    Salvando Novo 'Department'

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,15 +3,23 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
+	
+	private DepartmentService service;
 	
 	private Department entity;
 
@@ -34,14 +42,42 @@ public class DepartmentFormController implements Initializable{
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("A entidade estava NULA !");
+		}
+		if (service == null) {
+			throw new IllegalStateException("O serviço estava NULO !");
+		}
+		try {
+			entity = getFormData();
+			service.seveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} 
+		catch (DbException e) {
+			Alerts.ShowAlert("Erro ao Salvar Objeto", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+		
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
@@ -61,4 +97,6 @@ public class DepartmentFormController implements Initializable{
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
+	
+	
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -13,4 +13,13 @@ public class DepartmentService {
 	public List<Department> findAll(){
 		return dao.findAll();
 	}
+	
+	public void seveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
